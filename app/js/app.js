0633d2eeafc06fb7de2eba8954c30b5c6c79bc25
Helen Thi <ht5658@gmail.com>
2015-02-20 17:31:13 -0500

    wdi chat

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,21 @@
 'use strict';
+//code
+//github pages
+
+ var myDataRef = new Firebase('https://piu2nzkmbwm.firebaseio-demo.com/');
+$('#messageInput').keypress(function (e) {
+  if (e.keyCode == 13) {
+    var name = $('#nameInput').val();
+    var text = $('#messageInput').val();
+    myDataRef.push({name: name, text: text});
+    $('#messageInput').val('');
+  }
+});
+myDataRef.on('child_added', function(snapshot) {
+  var message = snapshot.val();
+  displayChatMessage(message.name, message.text);
+});
+function displayChatMessage(name, text) {
+  $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+  $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+};
