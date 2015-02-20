8123f565d24368e35cac78dbc3575584e771dad0
fishermanswharff <fishermanswharff@github.com>
2015-02-19 22:06:56 -0500

    working code

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,22 @@
-// Your code goes here
+'use strict';
+$(document).ready(function(){
+  var myDataRef = new Firebase('https://crackling-fire-3909.firebaseio.com/');
+  $('#messageInput').keypress(function (e) {
+    if (e.keyCode === 13) {
+      var name = $('#nameInput').val();
+      var text = $('#messageInput').val();
+      myDataRef.push({name: name, text: text});
+      $('#messageInput').val('');
+    }
+  });
+  myDataRef.on('child_added', function(snapshot) {
+    var message = snapshot.val();
+    displayChatMessage(message.name, message.text);
+  });
+  function displayChatMessage(name, text) {
+    if(name && text){
+      $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+      $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+    }
+  }
+});
